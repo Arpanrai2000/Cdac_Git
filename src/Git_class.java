95c9724c7c4bf36367ce1570b891ac2293339fde
arpanRai <raiarpan979@gmail.com>
2021-11-23 15:24:51 +0530

    Create a project..now we can add file in to it !!!!

---
+++ src/Git_class.java
@@ -0,0 +1,12 @@
+
+public class Git_class {
+	public static void main(String args[])
+	{
+		System.out.println("This is git project ");
+		System.out.println("Task done by lead... ");
+		System.out.println("HIII everyone!!!! ");	
+		
+		System.out.println("After stanging area ");	
+		
+	}
+}
